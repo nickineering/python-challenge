f1d7e7843c8668dcb78d05d5c3854cec7f604f4f
Nicholas Ferrara <nferrara100@gmail.com>
2018-10-12 13:01:14 +0100

    Working q4

--- q4.py
+++ q4.py
@@ -1,6 +1,24 @@
-def do_stuff(args):
-    return args
+import sys
+import requests
+from bs4 import BeautifulSoup as BS
+import json
+
+
+def crawl_links(urls):
+    results = {}
+    for url in urls:
+        results[url] = {'links': [], 'images': []}
+        page = requests.get(url)
+        soup = BS(page.text, 'html.parser')
+        
+        for link in soup.find_all('a'):
+            results[url]['links'].append(link.get('href'))
+        for image in soup.find_all('img'):
+            results[url]['images'].append(image.get('src'))
+
+    return results
 
 
 if __name__ == '__main__':
-    pass
+    links = crawl_links(sys.argv[1:])
+    print(json.dumps(links, sort_keys=True, indent=4))
