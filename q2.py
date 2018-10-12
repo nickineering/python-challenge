4617be6453adf6b2ea88dc670ce750482a150756
Nicholas Ferrara <nferrara100@gmail.com>
2018-10-12 03:11:22 +0100

    Q2 complete

--- q2.py
+++ q2.py
@@ -1,15 +1,20 @@
 from unittest import TestCase
 import unittest
+from collections.abc import Iterable
+
 
 data = [
     'one',
     ['one', 'two'],
-    {'one': 1, 'two': 2}
+    {'one': 1, 'two': 2},
+    1
 ]
 
 
-def to_list(input):
-    return input
+def to_list(args):
+    if isinstance(args, str) or not isinstance(args, Iterable):
+        args = [args]
+    return [*args]
 
 
 class ListTest(TestCase):
@@ -20,6 +25,9 @@ class ListTest(TestCase):
     def test_list_full(self):
         for datum in data:
             self.assertGreater(len(to_list(datum)), 0)
+
+    def test_string_unseparated(self):
+        self.assertEqual(to_list(data[0]), [data[0]])
 
 
 if __name__ == '__main__':
