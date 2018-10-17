782f0ea5973b272e47f1300d196fe130b1d581ed
Nicholas Ferrara <nferrara100@gmail.com>
2018-10-17 21:48:22 +0100

    Comments for question 4

--- q4.py
+++ q4.py
@@ -35,42 +35,58 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+# A fake header so that our request does not appear suspicious to the sites crawled
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko)'
                   'Chrome/53.0.2785.143 Safari/537.36'
 }
 
 
+# Crawl the provided list of URLs and extract their links and images. Returns the result in a dictionary.
 def crawl_links(urls):
+    # If only a single URL pack it into a list so that its string does not get separated by character.
     if not isinstance(urls, list):
         urls = [urls]
+    # Create an empty return result to add to later.
     results = {}
     for url in urls:
+        # Regardless of whether anything is found in this url create this data structure so reading it later is easier.
         results[url] = {'links': [], 'images': [], 'errors': []}
 
+        # Try to load the URL.
         try:
             page = requests.get(url, headers=headers, timeout=5)
+        # Store any exceptions in the errors list.
         except requests.exceptions.RequestException as e:
             results[url]['errors'].append([str(e)])
             return results
+        # Note if a request failed because the user quit.
         except KeyboardInterrupt:
             results[url]['errors'].append("Keyboard interrupt exception")
             return results
 
+        # If the page is not okay store its error code and return instead of parsing the error page.
         if page.status_code != 200:
             results[url]['errors'].append("HTTP status " + page.status_code)
             return results
 
+        # Parse the HTML.
         soup = BeautifulSoup(page.text, 'html.parser')
 
+        # Add the links and the images on the page to the final result.
         for link in soup.find_all('a'):
             results[url]['links'].append(link.get('href'))
         for image in soup.find_all('img'):
             results[url]['images'].append(image.get('src'))
 
+    # Return the final result.
     return results
 
 
+# Runs the example output if and only if the scripts is being executed directly. If it is imported do not run since
+# such output might then be an inconvenience.
 if __name__ == '__main__':
+    # Take the arguments given with the script when it is executed and use them to run crawl_links.
     links = crawl_links(sys.argv[1:])
+    # Print the output of crawl_links in JSON form.
     print(json.dumps(links, sort_keys=True, indent=4))
--- q4_tests.py
+++ q4_tests.py
@@ -1,26 +1,33 @@
+"""
+Tests question 4
+"""
 from unittest import TestCase
 import unittest
 from unittest import mock
 from q4 import crawl_links
 from q4 import headers
 
+# URLs to use in the test. These could be expanded upon if more elaborate tests were required in future.
 test_href = "https://example.com/"
 test_image = "/img/logo.jpg"
 test_search_url = "https://google.com/"
 
 
-# This method will be used by the mock to replace requests.get
+# This function will be used by the mock to replace requests.get. This prevents network usage so that the test always
+# works the same regardless of the state of the developer's network.
 def mocked_requests_get(*args, **kwargs):
+    # A very simple replacement of the result of requests.get.
     class MockResponse:
         def __init__(self, text, status_code):
             self.text = text
             self.status_code = status_code
 
+    # A test link and image are included so that their existence can be checked later.
     return MockResponse('I am a test page. <a href="' + test_href + '"/>My Link</a>. <img src="' + test_image +
                         '"> More text.', 200)
 
 
-# Our test case class
+# The class that conducts the tests.
 class TestCrawlLinks(TestCase):
 
     # We patch 'requests.get' with our own method. The mock object is passed in to our test case method.
@@ -29,24 +36,31 @@ class TestCrawlLinks(TestCase):
         self.data = crawl_links(test_search_url)
         self.mock_get = mock_get
 
+    # Checks that crawl_links returns a valid dictionary.
     def test_fetch_returns_dict(self):
         self.assertTrue(isinstance(self.data, dict))
 
+    # Checks that crawl_links fetches the URL it was supposed to fetch.
     def test_correct_url_searched(self):
         self.assertIn(mock.call(test_search_url, headers=headers, timeout=5), [*self.mock_get.call_args_list])
 
+    # Checks that the test link was found.
     def test_link_found(self):
         self.assertIn(test_href, self.data[test_search_url]["links"])
 
+    # Checks that the test image was found.
     def test_image_found(self):
         self.assertIn(test_image, self.data[test_search_url]["images"])
 
+    # Checks that extra links are not accidentally added to the result.
     def test_correct_num_links_found(self):
         self.assertEqual(1, len(self.data[test_search_url]["links"]))
 
+    # Checks that extra images are not accidentally added to the result.
     def test_correct_num_images_found(self):
         self.assertEqual(1, len(self.data[test_search_url]["images"]))
 
 
+# Runs the tests if this module is executed directly.
 if __name__ == '__main__':
     unittest.main()
